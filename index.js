c1b66559b6c369bb2621e303f7d8a1eea0d0f6e0
ItsRegby <shevchenko.eduard1121@vu.cdu.edu.ua>
2024-03-31 18:06:02 +0300

    registration

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const secretKey = 'secret';
 
 const app = express();
 const PORT = 3000;
 
 // Допоміжний об'єкт для збереження даних про квартири (типу БД)
 let apartments = [
-    { id: 1, address: '123 Shevchenka St', price: 200000, images: [], ownerEmail: 'owner1@example.com' },
-    { id: 2, address: '456 Lvivska St', price: 250000, images: [], ownerEmail: 'owner2@example.com' },
-    { id: 3, address: '789 Lesi Ukrainku St', price: 300000, images: [], ownerEmail: 'owner3@example.com' },
-    { id: 4, address: '101 V.Koguba St', price: 180000, images: [], ownerEmail: 'owner4@example.com' },
-    { id: 5, address: '222 Universytetska St', price: 220000, images: [], ownerEmail: 'owner5@example.com' }
+    { id: 1, address: '123 Shevchenka St', price: 200000, rooms: 3, images: [], ownerEmail: 'owner1@example.com', views: 0, isFeatured: true, isTop: false},
+    { id: 2, address: '456 Lvivska St', price: 250000, rooms: 2, images: [], ownerEmail: 'owner2@example.com', views: 0 , isFeatured: true, isTop: true},
+    { id: 3, address: '789 Lesi Ukrainku St', price: 300000, rooms: 4, images: [], ownerEmail: 'owner3@example.com', views: 0 , isFeatured: true, isTop: false},
+    { id: 4, address: '101 V.Koguba St', price: 180000, rooms: 1, images: [], ownerEmail: 'owner4@example.com', views: 0 , isFeatured: false, isTop: false},
+    { id: 5, address: '222 Universytetska St', price: 220000, rooms: 3, images: [], ownerEmail: 'owner5@example.com', views: 0 , isFeatured: true, isTop: true}
+];
+
+// Структура для збереження зареєстрованих користувачів
+let users = [
+    { email: 'owner1@example.com', password: 'password1', role: 'owner' },
+    { email: 'owner2@example.com', password: 'password2', role: 'realtor' },
+    { email: 'owner3@example.com', password: 'password3', role: 'buyer' },
+    { email: 'owner4@example.com', password: 'password4', role: 'realtor' },
+    { email: 'owner5@example.com', password: 'password5', role: 'buyer' }
 ];
 
 
@@ -18,18 +29,98 @@ app.use(bodyParser.json());
 
 module.exports = app;
 
+// Ендпоїнт реєстрації користувача
+app.post('/register', (req, res) => {
+    try {
+        const { email, password, role } = req.body;
+
+        // Перевірка чи вже існує користувач з такою ж електронною адресою
+        const existingUser = users.find(user => user.email === email);
+        if (existingUser) {
+            throw new Error('Користувач з такою електронною адресою вже існує');
+        }
+
+        // Додавання нового користувача
+        const newUser = { email, password, role };
+        users.push(newUser);
+
+        // Створення токену для нового користувача
+        const token = jwt.sign({ email, role }, secretKey);
+
+        res.status(201).json({ message: 'Користувач зареєстрований', token });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// Ендпоїнт автентифікації користувача
+app.post('/login', (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Пошук користувача за електронною адресою та паролем
+        const user = users.find(user => user.email === email && user.password === password);
+        if (!user) {
+            throw new Error('Неправильна електронна адреса або пароль');
+        }
+
+        // Створення токену для автентифікованого користувача з терміном життя 1 година
+        const token = jwt.sign({ email: user.email, role: user.role }, secretKey, { expiresIn: '1h' });
+
+        res.json({ message: 'Автентифікація успішна', token });
+    } catch (error) {
+        res.status(401).json({ message: error.message });
+    }
+});
+
+
+// Перевірка токену
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, secretKey, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
 // Отримати список квартир з можливістю фільтрації за ціною
 app.get('/apartments', (req, res) => {
     try {
-        const { maxPrice } = req.query;
-        let filteredApartments = apartments;
+        // Отримуємо параметри фільтрації з запиту
+        const { location, maxPrice, minRooms, isFeatured, isTop } = req.query;
 
+        // Копіюємо всі квартири у відфільтровані квартири (початково вони рівні усім квартирам)
+        let filteredApartments = [...apartments];
+
+        // Фільтруємо за розташуванням
+        if (location) {
+            filteredApartments = filteredApartments.filter(apartment => apartment.address.toLowerCase().includes(location.toLowerCase()));
+        }
+
+        // Фільтруємо за максимальною ціною
         if (maxPrice) {
             const parsedMaxPrice = parseInt(maxPrice);
-            if (isNaN(parsedMaxPrice)) {
-                throw new Error('Максимальна ціна повинна бути числом');
-            }
             filteredApartments = filteredApartments.filter(apartment => apartment.price <= parsedMaxPrice);
+        }
+
+        // Фільтруємо за мінімальною кількістю кімнат
+        if (minRooms) {
+            const parsedMinRooms = parseInt(minRooms);
+            filteredApartments = filteredApartments.filter(apartment => apartment.rooms >= parsedMinRooms);
+        }
+
+        // Фільтруємо за флажком isFeatured
+        if (isFeatured) {
+            filteredApartments = filteredApartments.filter(apartment => apartment.isFeatured === (isFeatured === 'true'));
+        }
+
+        // Фільтруємо за флажком isTop
+        if (isTop) {
+            filteredApartments = filteredApartments.filter(apartment => apartment.isTop === (isTop === 'true'));
         }
 
         res.json(filteredApartments);
@@ -53,6 +144,8 @@ app.get('/apartments/:id', (req, res) => {
             throw new Error('Ідентифікатор квартири повинен бути числом');
         }
 
+        apartments[index].views++;
+
         const apartment = apartments.find(apartment => apartment.id === id);
         if (apartment) {
             res.json(apartment);
@@ -65,9 +158,9 @@ app.get('/apartments/:id', (req, res) => {
 });
 
 // Створити нову квартиру
-app.post('/apartments', (req, res) => {
+app.post('/apartments', authenticateToken, (req, res) => {
     try {
-        const { address, price, images, ownerEmail } = req.body;
+        const { address, price, rooms, images, ownerEmail,isFeatured,isTop } = req.body;
 
         if (!address || typeof address !== 'string') {
             throw new Error('Адреса квартири відсутня або має невірний формат');
@@ -77,18 +170,34 @@ app.post('/apartments', (req, res) => {
             throw new Error('Ціна квартири відсутня або має невірний формат');
         }
 
+        if (!rooms || typeof rooms !== 'number' || isNaN(rooms)) {
+            throw new Error('Кількість кімнат відсутня або має невірний формат');
+        }
+
         if (!ownerEmail || typeof ownerEmail !== 'string' || !validateEmail(ownerEmail)) {
             throw new Error('Електронна пошта власника квартири відсутня або має невірний формат');
         }
 
-        const id = apartments.length + 1;
-        const newApartment = { id, address, price, images: Array.isArray(images) ? images : [], ownerEmail };
+        // Створення нової квартири з вказаними даними
+        const newApartment = {
+            id: apartments.length + 1,
+            address,
+            price,
+            rooms,
+            images: Array.isArray(images) ? images : [],
+            ownerEmail,
+            views: 0,
+            isFeatured: isFeatured || false,
+            isTop: isTop || false
+        };
+
         apartments.push(newApartment);
         res.status(201).json(newApartment);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
 });
+
 
 // Валідація електронної пошти
 function validateEmail(email) {
@@ -97,7 +206,7 @@ function validateEmail(email) {
 }
 
 // Завантажити нове зображення для квартири
-app.post('/apartments/:id/images', (req, res) => {
+app.post('/apartments/:id/images', authenticateToken, (req, res) => {
     try {
         const id = parseInt(req.params.id);
         const index = apartments.findIndex(apartment => apartment.id === id);
@@ -126,7 +235,7 @@ app.post('/apartments/:id/images', (req, res) => {
 
 
 // Відправити повідомлення власнику квартири
-app.post('/apartments/:id/message', (req, res) => {
+app.post('/apartments/:id/message', authenticateToken, (req, res) => {
     try {
         const id = parseInt(req.params.id);
         const index = apartments.findIndex(apartment => apartment.id === id);
@@ -158,10 +267,10 @@ app.post('/apartments/:id/message', (req, res) => {
 
 
 // Оновити інформацію про квартиру
-app.put('/apartments/:id', (req, res) => {
+app.put('/apartments/:id', authenticateToken, (req, res) => {
     try {
         const id = parseInt(req.params.id);
-        const { address, price, images, ownerEmail } = req.body;
+        const { address, price, rooms, images, ownerEmail, isFeatured, isTop } = req.body;
 
         const index = apartments.findIndex(apartment => apartment.id === id);
         if (index === -1) {
@@ -169,28 +278,44 @@ app.put('/apartments/:id', (req, res) => {
             return; // Повертаємо з функції, оскільки квартира не знайдена
         }
 
-        apartments[index] = { ...apartments[index], address, price, images, ownerEmail };
+        apartments[index] = { ...apartments[index], address, price, rooms, images, ownerEmail, isFeatured: isFeatured || false, isTop: isTop || false };
         res.json(apartments[index]);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
 });
 
-// Видалити квартиру за її ідентифікатором
-app.delete('/apartments/:id', (req, res) => {
+// Видалення квартири за її ідентифікатором, з умовою авторизації
+app.delete('/apartments/:id', authenticateToken, (req, res) => {
     try {
         const id = parseInt(req.params.id);
+        const user = req.user; // Отримуємо користувача з токену
 
         const index = apartments.findIndex(apartment => apartment.id === id);
         if (index === -1) {
             res.status(404).json({ message: 'Квартира не знайдена' });
-            return; // Повертаємо з функції, оскільки квартира не знайдена
+            return;
+        }
+
+        // Перевірка, чи користувач має право видалити цю квартиру
+        if (apartments[index].ownerEmail !== user.email) {
+            res.status(403).json({ message: 'Немає прав на видалення цієї квартири' });
+            return;
         }
 
         apartments.splice(index, 1);
         res.json({ message: 'Квартира видалена' });
     } catch (error) {
         res.status(404).json({ message: error.message });
+    }
+});
+
+
+app.get('/users', (req, res) => {
+    try {
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: 'Помилка сервера' });
     }
 });
 
